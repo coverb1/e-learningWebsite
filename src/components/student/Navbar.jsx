ac68932c2020efa0324d15a6d78097df825ea43a
Mucyobruce <cboy85096@email.com>
2026-03-22 20:17:37 +0200

    changes

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk,UserButton,useUser } from '@clerk/react'
 
 const Navbar = () => {
+
+  const{openSignIn}=useClerk()
+  const {user}=useUser()
 
   //“Does the URL have /course-list inside it?
   const isCourseListPage = location.pathname.includes('/course-list')
@@ -14,17 +18,26 @@ const Navbar = () => {
 
       <div className='hidden md:flex items-center gap-5 text-gray-500'>
         <div>
+
+         { user && 
+         <>
           <button>Become Educator </button>
           <Link to='/my-enrlloment'>|| My-enrlloment</Link>
+          </>
+          }
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        
+       {
+        user? <UserButton/> :
+        <button onClick={()=>openSignIn()} 
+        className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
       </div>
 
       {/* for phone screens */}
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-400'>
         <div>
-          <button>Become Educator </button>
-          <Link to='/my-enrlloment'>|| My-enrlloment</Link>
+    <button>Become Educator </button>
+    <Link to='/my-enrlloment'>|| My-enrlloment</Link>
         </div>
         <button><img src={assets.user_icon} alt="" /></button>
       </div>
