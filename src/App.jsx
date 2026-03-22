5a349d242d9b6dfa1a3dcf62e538c59eea0d9949
Mucyobruce <cboy85096@email.com>
2026-03-22 10:07:54 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/students/Home'
+import CourseList from './pages/students/CourseList'
+import CourseDetails from './pages/students/CourseDetails'
+import Myenrlloment from './pages/students/Myenrlloment'
+import Player from './pages/students/Player'
+import Loading from './components/student/Loading'
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import Mycourse from './pages/educator/Mycourse'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+
 
 const App = () => {
   return (
     <div>
-        <p className=''></p>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/course-list' element={<CourseList />} />
+        {/* <Route path='/course-list' element={<CourseList />} /> */}
+        <Route path='/course-list/:input' element={<CourseList />} />
+        <Route path='/course/:id' element={<CourseDetails />} />
+        <Route path='/my-enrlloment' element={<Myenrlloment />} />
+        <Route path='/player/:courseId' element={<Player />} />
+        <Route path='/loading/:path' element={<Loading />} />
+
+        <Route path='/educator' element={<Educator/>}>
+            <Route path='educator' element={<Dashboard/>} />
+            <Route path='add-course' element={<AddCourse/>} />
+            <Route path='my-courses' element={<Mycourse/>} />
+            <Route path='student-enrlled' element={<StudentsEnrolled/>} />
+        </Route>
+
+      </Routes>
     </div>
   )
 }
--- src/context/Appcontext.jsx
+++ src/context/Appcontext.jsx
@@ -1,1 +1,17 @@
+import { createContext } from "react";
 
+export const AppContext=createContext()
+
+
+export const AppContextProvider=(prop)=>{
+
+    const value={
+
+    }
+
+return(
+    <AppContext.Provider value={value}>
+        {prop.children}
+    </AppContext.Provider>
+)
+}
