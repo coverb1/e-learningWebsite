bbff3390da190d3e2fcf441ae8a4c0fc910e760c
Mucyobruce <cboy85096@email.com>
2026-03-25 17:52:07 +0200

    changes

--- src/components/student/Hero.jsx
+++ src/components/student/Hero.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 
     px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
       <h1 className='md:text-home-heading-large text-home-heading-small relative 
-      font-bold text-gray-800 max-w-3xl mx-auto'>Empower your future  with the courses designed to <span 
-      className='text-blue-200'>fit your choice</span>
-      <img src={assets.sketch} className='md:block absolute -bottom-5' alt="" /></h1>
+      font-bold text-gray-800 max-w-3xl mx-auto'>Empower your future  with the courses designed to <span
+          className='text-blue-200'>fit your choice</span>
+        <img src={assets.sketch} className='md:block absolute -bottom-5' alt="" /></h1>
 
       <p className='md:block hidden text-gray-500 max-w-2xl mx-auto'>we bring together world class instructors, interactive,content
         and supportive community  to help to achieve you personal and proffessional goals.
       </p>
 
       <p className='md:hidden text-gray-500 mx-w-sm mx-auto'>We bring together world-class instructor to help to achieve your proffessional goals</p>
+      <SearchBar />
     </div>
   )
 }
