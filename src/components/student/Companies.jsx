8fec473c082808370bb8c493d38a9158d4d9ca7e
Mucyobruce <cboy85096@email.com>
2026-03-25 18:45:18 +0200

    changes

--- src/components/student/Companies.jsx
+++ src/components/student/Companies.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Companies = () => {
   return (
-    <div>Companies</div>
+    <div className='mt-5'>
+      <p className='  text-xl md:text-3xl md: text-gray-600'>
+        Learn to change The World</p>
+    </div>
   )
 }
 
--- src/components/student/CourseSection.jsx
+++ src/components/student/CourseSection.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CourseSection = () => {
   return (
-    <div>CourseSection</div>
+    <div className='flex flex-col gap-10'>
+      <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
+      <p>Discover our  top-rated cross various categories.
+        from coding and design to business and wellnes, oyr courses are crafted to deliver
+        results
+      </p>
+      <Link to={'/course-list/'} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded' onClick={()=>scrollTo(0,0)}>
+      Show all course
+      </Link>
+    </div>
   )
 }
 
