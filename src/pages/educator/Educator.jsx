be7ecd9220ed3df9ec8a162058dcbcc6d2926f79
Mucyobruce <cboy85096@gmail.com>
2026-04-03 11:20:57 +0200

    changes

--- src/pages/educator/Educator.jsx
+++ src/pages/educator/Educator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
 
 const Educator = () => {
   return (
-    <div>
-      <p>Educator Page</p>
+    <div className=''>
+      <Navbar/>
       <div>
 {<Outlet/>}
       </div>
